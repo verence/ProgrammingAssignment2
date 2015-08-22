6a2a636b10c73f5df7647e6647eb556e93769da1
verence <st.gneist@googlemail.com>
2015-08-22 23:39:06 +0200

    init commit -  no change

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+# todo 
 }
 
 
@@ -12,4 +12,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+# todo
 }
